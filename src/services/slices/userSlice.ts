e153bd0525635b01758c69fac8afb84dca934c59
Akbota <kaliollaeva.b@gmail.com>
2025-08-24 13:14:55 +0500

    feat(user): add user registration and authorization

---
+++ src/services/slices/userSlice.ts
@@ -0,0 +1,93 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { loginUserApi, registerUserApi, TRegisterData } from '@api';
+import { TUser } from '@utils-types';
+import { setCookie } from '../../utils/cookie';
+
+type State = {
+  isAuthChecked: boolean;
+  isAuthenticated: boolean;
+  data: TUser | null;
+  loginUserError: unknown | null;
+  registerUserError: unknown | null;
+  loginUserRequest: unknown | null;
+  registerUserRequest: boolean;
+};
+
+const initialState: State = {
+  isAuthChecked: false,
+  isAuthenticated: false,
+  data: null,
+  loginUserError: null,
+  registerUserError: null,
+  loginUserRequest: false,
+  registerUserRequest: false
+};
+
+export const registerUser = createAsyncThunk(
+  'user/registerUser',
+  async ({ email, password, name }: TRegisterData, { rejectWithValue }) => {
+    const data = await registerUserApi({ email, password, name });
+
+    if (!data.success) {
+      return rejectWithValue(data);
+    }
+
+    return data.user;
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  'user/loginUser',
+  async (
+    { email, password }: Omit<TRegisterData, 'name'>,
+    { rejectWithValue }
+  ) => {
+    const data = await loginUserApi({ email, password });
+
+    if (!data?.success) {
+      return rejectWithValue(data);
+    }
+
+    setCookie('accessToken', data.accessToken);
+    localStorage.setItem('refreshToken', data.refreshToken);
+    return data.user;
+  }
+);
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.registerUserRequest = true;
+        state.registerUserError = null;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.registerUserRequest = false;
+        state.registerUserError = action.payload;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.registerUserRequest = false;
+        state.isAuthenticated = true;
+      });
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.loginUserRequest = true;
+        state.loginUserError = null;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.loginUserRequest = false;
+        state.loginUserError = action.payload;
+        state.isAuthChecked = true;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.loginUserRequest = false;
+        state.isAuthenticated = true;
+        state.isAuthChecked = true;
+      });
+  }
+});
