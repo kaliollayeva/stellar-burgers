6a9dc81acd918420b3b88f67a97c7390df83415c
Akbota <kaliollaeva.b@gmail.com>
2025-08-25 18:29:29 +0500

    feat(profile): add logout and redirect functionality

--- src/components/profile-menu/profile-menu.tsx
+++ src/components/profile-menu/profile-menu.tsx
@@ -1,11 +1,17 @@
 import { FC } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { ProfileMenuUI } from '@ui';
+import { useDispatch } from '../../services/store';
+import { deleteUser } from '../../services/slices/userSlice';
 
 export const ProfileMenu: FC = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { pathname } = useLocation();
 
-  const handleLogout = () => {};
+  const handleLogout = () => {
+    dispatch(deleteUser()).then(() => navigate('/login'));
+  };
 
   return <ProfileMenuUI handleLogout={handleLogout} pathname={pathname} />;
 };
