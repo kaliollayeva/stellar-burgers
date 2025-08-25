a762cd029f168385a591269547aa8963c78beee0
Akbota <kaliollaeva.b@gmail.com>
2025-08-25 20:24:48 +0500

    feat(orders): add orders slice

--- src/services/rootReducer.ts
+++ src/services/rootReducer.ts
@@ -3,9 +3,11 @@ import { combineReducers } from '@reduxjs/toolkit';
 import { ingredientsSlice } from './slices/ingredientsSlice';
 import { constructorSlice } from './slices/constructorSlice';
 import { userSlice } from './slices/userSlice';
+import { ordersSlice } from './slices/ordersSlice';
 
 export const rootReducer = combineReducers({
   ingredients: ingredientsSlice.reducer,
   constructorData: constructorSlice.reducer,
-  user: userSlice.reducer
+  user: userSlice.reducer,
+  orders: ordersSlice.reducer
 });
---
+++ src/services/slices/ordersSlice.ts
@@ -0,0 +1,61 @@
+import { getOrdersApi, orderBurgerApi } from '@api';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { TOrder } from '@utils-types';
+
+type State = {
+  orderRequest: boolean;
+  order: TOrder | null;
+  ordersList: TOrder[];
+};
+
+const initialState: State = {
+  orderRequest: false,
+  order: null,
+  ordersList: []
+};
+
+export const createOrder = createAsyncThunk(
+  'orders/create',
+  async (ingredientIds: string[]) => {
+    const res = await orderBurgerApi(ingredientIds);
+    return res.order;
+  }
+);
+
+export const getOrders = createAsyncThunk('orders/get', async () => {
+  const res = await getOrdersApi();
+  return res;
+});
+
+export const ordersSlice = createSlice({
+  name: 'orders',
+  reducers: {
+    clearOrderData: (state) => {
+      state.order = null;
+    },
+    clearOrders: (state) => {
+      state.ordersList = [];
+    }
+  },
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(createOrder.pending, (state) => {
+        state.orderRequest = true;
+      })
+      .addCase(createOrder.fulfilled, (state, action) => {
+        state.order = action.payload;
+        state.orderRequest = false;
+      })
+      .addCase(createOrder.rejected, (state) => {
+        state.orderRequest = false;
+      })
+
+      /* Orders */
+      .addCase(getOrders.fulfilled, (state, action) => {
+        state.ordersList = action.payload;
+      });
+  }
+});
+
+export const { clearOrderData, clearOrders } = ordersSlice.actions;
