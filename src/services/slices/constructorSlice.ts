74d247874cc830720b78047514a42a58e79e8ffc
Akbota <kaliollaeva.b@gmail.com>
2025-08-31 00:35:40 +0500

    feat(constructor): add unique id when adding an ingredient

--- src/services/slices/constructorSlice.ts
+++ src/services/slices/constructorSlice.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
 import { TConstructorIngredient, TOrder } from '@utils-types';
 import { orderBurgerApi } from '../../utils/burger-api';
+import { v4 as uuidv4 } from 'uuid';
 
 type ConstructorState = {
   constructorItems: {
@@ -24,8 +25,13 @@ export const constructorSlice = createSlice({
   name: 'constructor',
   initialState,
   reducers: {
-    addIngredient: (state, action: PayloadAction<TConstructorIngredient>) => {
-      state.constructorItems.ingredients.push(action.payload);
+    addIngredient: {
+      reducer: (state, action: PayloadAction<TConstructorIngredient>) => {
+        state.constructorItems.ingredients.push(action.payload);
+      },
+      prepare: (ingredient: TConstructorIngredient) => ({
+        payload: { ...ingredient, id: uuidv4() }
+      })
     },
     removeIngredient: (state, action: PayloadAction<string>) => {
       state.constructorItems.ingredients =
